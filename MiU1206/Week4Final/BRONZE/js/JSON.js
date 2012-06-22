ec0e5eb51cc60bf7e5ad103b0f36786601f2d116
Cheryl <MsCheryl@fullsail.edu>
2012-06-22 07:04:45 -0400

    Fixing Json error

--- MiU1206/Week4Final/BRONZE/js/JSON.js
+++ MiU1206/Week4Final/BRONZE/js/JSON.js
@@ -6,146 +6,147 @@
 function autoFillData() {
    var json = {
 	"member1": {
-		"ministryInfo": ["Select Ministry:", "Leadership"],
-		"fname": ["First Name:", "Susie"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Women"],
+		"fName": ["First Name:", "Susie"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	},
 	"member2": {
-		"ministryInfo": ["Select Ministry:", "Leadership"],
-		"fname": ["First Name:", "Becky"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Seniors"],
+		"fName": ["First Name:", "Becky"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "beckbeck@mail.com"],
 
 	},
 	"member3": {
-		"ministryInfo": ["Select Minstry:", "Leadership"],
-		"fname": ["First Name:", "Sue"],
-		"lname": ["Last Name:", "Turner"],
+		"groups": ["Select Minstry:", "Women"],
+		"fName": ["First Name:", "Sue"],
+		"lName": ["Last Name:", "Turner"],
 		"email": ["Email:", "trunsue@rmail.com"],
 
 	},
 	"member4": {
-		"ministryInfo": ["Select Ministry:", "Ministries"],
-		"fname": ["First Name:", "Marquis"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Men"],
+		"fName": ["First Name:", "Marquis"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	},
 	"member5": {
-		"ministryInfo": ["Select Ministry:", "Ministries"],
-		"fname": ["First Name:", "Johnny"],
-		"lname": ["Last Name:", "Walker"],
+		"groups": ["Select Ministry:", "Men"],
+		"fName": ["First Name:", "Johnny"],
+		"lName": ["Last Name:", "Walker"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	
 	},
 	"member6": {
-		"ministryInfo": ["Select Ministry:", "Ministries"],
-		"fname": ["First Name:", "Mykal"],
-		"lname": ["Last Name:", "Walker"],
+		"groups": ["Select Ministry:", "Teen"],
+		"fName": ["First Name:", "Mykal"],
+		"lName": ["Last Name:", "Walker"],
 		"email": ["Email:", "actjmd@aol.com"],
 
 	},
 	"member7": {
-		"ministryInfo": ["Select Ministry:", "Lesson"],
-		"fname": ["First Name:", "Susie"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Teen"],
+		"fName": ["First Name:", "Susie"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	},
 	"member8": {
-		"ministryInfo": ["Select Ministry:", "Lesson"],
-		"fname": ["First Name:", "Becky"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Children"],
+		"fName": ["First Name:", "Becky"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "beckbeck@mail.com"],
 		
 	},
 	"member9": {
-		"ministryInfo": ["Select Minstry:", "Upcoming"],
-		"fname": ["First Name:", "Sue"],
-		"lname": ["Last Name:", "Turner"],
+		"groups": ["Select Minstry:", "Children"],
+		"fName": ["First Name:", "Sue"],
+		"lName": ["Last Name:", "Turner"],
 		"email": ["Email:", "trunsue@rmail.com"],
 	
 	},
 	"member10": {
-		"ministryInfo": ["Select Ministry:", "Upcoming"],
-		"fname": ["First Name:", "Marquis"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Teen"],
+		"fName": ["First Name:", "Marquis"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	},
 	"member11": {
-		"ministryInfo": ["Select Ministry:", "Lesson"],
-		"fname": ["First Name:", "Johnny"],
-		"lname": ["Last Name:", "Walker"],
+		"groups": ["Select Ministry:", "Children"],
+		"fName": ["First Name:", "Johnny"],
+		"lName": ["Last Name:", "Walker"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	
 	},
 	"member12": {
-		"ministryInfo": ["Select Ministry:", "Lesson"],
-		"fname": ["First Name:", "Mykal"],
-		"lname": ["Last Name:", "Walker"],
+		"groups": ["Select Ministry:", "Senior"],
+		"fName": ["First Name:", "Mykal"],
+		"lName": ["Last Name:", "Walker"],
 		"email": ["Email:", "actjmd@aol.com"],
 
 	},
 	"member13": {
-		"ministryInfo": ["Select Ministry:", "Leadership"],
-		"fname": ["First Name:", "Susie"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Women"],
+		"fName": ["First Name:", "Susie"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	},
 	"member14": {
-		"ministryInfo": ["Select Ministry:", "Leadership"],
-		"fname": ["First Name:", "Becky"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Senior"],
+		"fName": ["First Name:", "Becky"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "beckbeck@mail.com"],
 
 	},
 	"member15": {
-		"ministryInfo": ["Select Minstry:", "Leadership"],
-		"fname": ["First Name:", "Sue"],
-		"lname": ["Last Name:", "Turner"],
+		"groups": ["Select Minstry:", "Teen"],
+		"fName": ["First Name:", "Sue"],
+		"lName": ["Last Name:", "Turner"],
 		"email": ["Email:", "trunsue@rmail.com"],
 
 	},
 	"member16": {
-		"ministryInfo": ["Select Ministry:", "Upcoming"],
-		"fname": ["First Name:", "Marquis"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Men"],
+		"fName": ["First Name:", "Marquis"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	},
 	"member17": {
-		"ministryInfo": ["Select Ministry:", "Upcoming"],
-		"fname": ["First Name:", "Johnny"],
-		"lname": ["Last Name:", "Walker"],
+		"groups": ["Select Ministry:", "Men"],
+		"fName": ["First Name:", "Johnny"],
+		"lName": ["Last Name:", "Walker"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	
 	},
 	"member18": {
-		"ministryInfo": ["Select Ministry:", "Membership"],
-		"fname": ["First Name:", "Mykal"],
-		"lname": ["Last Name:", "Walker"],
+		"groups": ["Select Ministry:", "Children"],
+		"fName": ["First Name:", "Mykal"],
+		"lName": ["Last Name:", "Walker"],
 		"email": ["Email:", "actjmd@aol.com"],
 
 	},
 	"member19": {
-		"ministryInfo": ["Select Ministry:", "Membership"],
-		"fname": ["First Name:", "Susie"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Women"],
+		"fName": ["First Name:", "Susie"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "susiepoo@yourmail.com"],
 
 	},
 	"member20": {
-		"ministryInfo": ["Select Ministry:", "Ministries"],
-		"fname": ["First Name:", "Becky"],
-		"lname": ["Last Name:", "Williams"],
+		"groups": ["Select Ministry:", "Senior"],
+		"fName": ["First Name:", "Becky"],
+		"lName": ["Last Name:", "Williams"],
 		"email": ["Email:", "beckbeck@mail.com"],
 
-    }
+  }
+}
 }
